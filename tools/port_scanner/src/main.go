45e34f993e8a7540a905cd744fd7f0a75ba463ae
João Vitor Espig <jotinha1300@gmail.com>
2022-02-23 16:01:39 -0300

    The functions 'run' 'scanPort' and 'printPorts' have been passed to port_scanner.go

--- tools/port_scanner/src/main.go
+++ tools/port_scanner/src/main.go
@@ -2,19 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"os"
-	"sort"
 	"strconv"
-	"sync"
-	"time"
-
-	"github.com/TwiN/go-color"
-)
-
-const (
-	PORT_MIN_D = 1
-	PORT_MAX_D = 1024
 )
 
 func main() {
@@ -55,45 +44,4 @@ func main() {
 	}
 
 	run(host, iPort, fPort)
-}
-
-func run(host string, iPort uint64, fPort uint64) {
-	var i, j int
-	var openPorts []int
-	wg := &sync.WaitGroup{}
-
-	fmt.Println(host)
-	for i = int(iPort); i <= int(fPort); i += 100 {
-		iP := i       // initial port
-		fP := i + 100 // final port
-		wg.Add(100)
-		for j = iP; j < fP; j++ {
-			port := j
-			go func() {
-				if scanPort(host, port) {
-					openPorts = append(openPorts, port)
-				}
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-	}
-	sort.Ints(openPorts)
-	printPorts(openPorts)
-}
-
-func scanPort(host string, port int) bool {
-	addr := fmt.Sprintf("%s:%s", host, strconv.Itoa(port))
-	conn, err := net.DialTimeout("tcp", addr, 1*time.Second)
-	if err != nil {
-		return false
-	}
-	defer conn.Close()
-	return true
-}
-
-func printPorts(ports []int) {
-	for _, port := range ports {
-		fmt.Println(color.Ize(color.Cyan, strconv.Itoa(port)))
-	}
 }
